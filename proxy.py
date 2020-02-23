77320c04aca04b06688df28b84f8b32d874e20dd
kamilprz <kamil.prz@gmail.com>
2020-02-23 20:12:06 +0000

    added functions to tkinter, set up socket connection

--- proxy.py
+++ proxy.py
@@ -5,19 +5,56 @@ from tkinter import*
 
 # dict for blocked URLs		
 blocked = {}	
+# dict for cache
+cache = {}
+BUFFER_SIZE = 4096
 
 #tkinter - GUI used to dynamically block and unlock URLs
 def tkinter():
 	console = tk.Tk()
+
+	def block_url():
+		ret = block.get()
+		temp = blocked.get(ret)
+		if temp is None:
+			blocked[ret] = 1	
+			print(">> Blocked: " + ret)
+		else:
+			print(">> Already blocked")
+
 	block = Entry(console)
-	block.grid(row=0,column=0)
-	block_button = Button(console, text="Block URL")
-	block_button.grid(row=0, column=1)
+	block.pack()
+	block_button = Button(console, text = "Block URL", command = block_url)
+	block_button.pack()
+
+	def unblock_url():
+		ret = unblock.get()
+		temp = blocked.get(ret)
+		if temp is None:
+			print(">> " + ret + " is not blocked")
+		else:
+			blocked.pop(ret)
+			print(">> Unblocked: " + ret)
 
 	unblock = Entry(console)
-	unblock.grid(row=1, column=0)
-	unblock_button = Button(console, text="Unblock URL")
-	unblock_button.grid(row=1, column=1)
+	unblock.pack()
+	unblock_button = Button(console, text = "Unblock URL", command = unblock_url)
+	unblock_button.pack()
+
+	# prints all blocked urls
+	def print_blocked():
+		print(blocked)
+
+	print_blocked = Button(console, text = "Print Blocked URLs", command = print_blocked)
+	print_blocked.pack()
+
+	# prints all cached urls
+	def print_cache():
+		for key, value in cache.iteritems():
+			print(key)
+
+	print_cache = Button(console, text = "Print Cache", command = print_cache)
+	print_cache.pack()
 
 	mainloop()
 	
@@ -27,9 +64,32 @@ def main():
 	_thread.start_new_thread(tkinter,())
 
 	# user input port number
-	listening_port = int(input(">> Enter Listening Port Number: "))
+	port = int(input(">> Enter Listening Port Number: "))
 
+	try:
+		# Ininitiate socket
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
+		# bind socket to port
+		s.bind(('', port))						
+		s.listen()						
+		print(">> Initializing sockets...")
+		print(">> Server boot successful [ {0} ]\n".format(port))		
+	except Exception:
+		print(">> Error")
+		sys.exit(2)
 	
+	while True:
+		try:
+			# connection from browser
+			conn, client_addr = s.accept()		
+			# receive data
+			data = conn.recv(BUFFER_SIZE)		
+			# start thread
+			_thread.start_new_thread(proxy_thread, (conn, data, client_addr)) 
+		except KeyboardInterrupt:
+			s.close()
+			sys.exit(1)
+	s.close()
 
 if __name__ == '__main__':
 	main()
