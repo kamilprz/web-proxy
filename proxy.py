d9af0828b1e8655e910a3e1d2abd267f658fb8d7
kamilprz <kamil.prz@gmail.com>
2020-02-23 19:34:51 +0000

    added buttons to gui, taking input from user for port

--- proxy.py
+++ proxy.py
@@ -3,22 +3,33 @@ import os, sys, _thread, socket, time
 import tkinter as tk
 from tkinter import*
 
+# dict for blocked URLs		
+blocked = {}	
 
 #tkinter - GUI used to dynamically block and unlock URLs
 def tkinter():
 	console = tk.Tk()
 	block = Entry(console)
-	block.pack()
-	unblock = Entry(console)
-	unblock.pack()
+	block.grid(row=0,column=0)
+	block_button = Button(console, text="Block URL")
+	block_button.grid(row=0, column=1)
 
+	unblock = Entry(console)
+	unblock.grid(row=1, column=0)
+	unblock_button = Button(console, text="Unblock URL")
+	unblock_button.grid(row=1, column=1)
+
+	mainloop()
+	
 # MAIN PROGRAM
 def main():
-	# Run a thread of our tkinter function..
+	# boot up the tkinter gui
 	_thread.start_new_thread(tkinter,())
+
+	# user input port number
+	listening_port = int(input(">> Enter Listening Port Number: "))
 
 	
 
 if __name__ == '__main__':
 	main()
-
